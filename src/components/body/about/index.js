b09a835a15552e16b2199a0a6c5b5b2afe95dc3b
sintayehu1993 <sintayeh@buffalo.edu>
2021-10-07 23:03:14 -0400

    added a second project

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import "./about.css"
-import myPicture from './aboutMeImag.JPG'
-import SocialContact from '../../common/social-contact/index'
-import Separator from '../../common/separator'; 
+import React from 'react';
+import './about.css';
+import myPicture from './aboutMeImag.JPG';
+import SocialContact from '../../common/social-contact/index';
+import Separator from '../../common/separator';
 function About() {
-    return (
-        <div className="about">
-        <Separator />
-        <label className="section-title">About</label>
-           <div className="about-top">
-           <div className="about-info">I am a <span className="info-intro">SOFTWARE ENGINEER </span>specializing in JavaScript, NodeJS, React/Redux, PostgreSQL, HTML/CSS & more. 
-           <br /> I'm always finding ways to improve my skills, whether that's adding features to my existing projects, developing new ones from scratch, or learning a new language/library/framework.</div>
-           <div className="about-photo">
-           <img alt="" className="my-picture" src={myPicture}></img>
-           </div>
-           </div>
-           <SocialContact />
+  return (
+    <div className="about">
+      <Separator />
+      <label className="section-title">About</label>
+      <div className="about-top">
+        <div className="about-info">
+          I am a <span className="info-intro">SOFTWARE ENGINEER </span>
+          specializing in JavaScript, NodeJS, React/Redux, PostgreSQL, HTML/CSS
+          & more. I'm always finding ways to <br /> improve my skills, whether
+          that's adding features to my existing projects, <br /> developing new
+          ones from scratch, or learning a new language/library/framework.
         </div>
-    )
+        <div className="about-photo">
+          <img alt="" className="my-picture" src={myPicture}></img>
+        </div>
+      </div>
+      <SocialContact />
+    </div>
+  );
 }
 
 //I'm always looking to improve my skills, whether that's adding features to my existing projects, developing new ones from scratch, or learning a new language/library/framework
 
-export default About
+export default About;
--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,7 +1,7 @@
 export const projectData=[ { 
 id:1,
 title: "Two-Plus",
-descriptions:"A live pair-programming tool and social platform for developers that includes discussion threads, live chat, follower/following systems, content upvoting and tagging, a dynamically loaded home page, and of course, collaborative code editing. Built with React, Google Firebase/Firestore, Ace Editor, and Material UI.",
+descriptions:"A live pair-programming tool and social platform for developers that includes discussion threads, live chat, follower/following systems, content upvoting and tagging, a dynamically loaded home page, and of course, collaborative code editing. Built with...",
 tags:["ReactJs", "Google Firebase/Firestore", "Ace Editor", "Material UI"],
 demo: "https://two-plus.web.app/guesthome",
 github:"https://github.com/MasterCodersInc/TwoPlus", 
@@ -9,11 +9,11 @@ image: require("/Users/mati/Desktop/2011-SR-Phase/Portfolio/2ndPortfolio/main-po
 },
 { 
     id:2,
-    title: "2++",
-    descriptions:"A live pair-programming tool and social platform for developers that includes discussion threads, live chat, follower/following systems, content upvoting and tagging, a dynamically loaded home page, and of course, collaborative code editing. Built with React, Google Firebase/Firestore, Ace Editor, and Material UI.",
-    tags:["ReactJs", "Google Firebase/Firestore", "Ace Editor", "Material UI"],
+    title: "CoinChaser",
+    descriptions:"A cryptocurrency price tracker, it gives you the ability to add coins to your watch list or remove. Clicking any of the coins will take you to a detail page. Which displays a historical data about the coin. Built with React hooks, Context API,  Fetching Data from API, React Router, and LocalStorage",
+    tags:["ReactJs", "Chart.js", "CSS/Bootstrap", "CoinGecko API"],
     demo: "https://two-plus.web.app/guesthome",
-    github:"https://github.com/MasterCodersInc/TwoPlus", 
-    image: require("/Users/mati/Desktop/2011-SR-Phase/Portfolio/2ndPortfolio/main-portfolio/src/assets/icons/projectsImges/TwoPlus.jpng").default,
+    github:"https://github.com/sintayehu1993/Stackathon-CoInChaser", 
+    image: require("/Users/mati/Desktop/2011-SR-Phase/Portfolio/2ndPortfolio/main-portfolio/src/assets/icons/projectsImges/CoinChaser.png").default,
     }
 ]
