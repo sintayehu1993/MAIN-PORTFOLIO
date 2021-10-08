af4e188cae18fd95911ee247d758a9ee2acdabb9
sintayehu1993 <sintayeh@buffalo.edu>
2021-10-07 20:41:58 -0400

    final touch

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import "./about.css"
 import myPicture from './aboutMeImag.JPG'
 import SocialContact from '../../common/social-contact/index'
+import Separator from '../../common/separator'; 
 function About() {
     return (
         <div className="about">
+        <Separator />
+        <label className="section-title">About</label>
            <div className="about-top">
-           <div className="about-info">I am <span className="info-intro">A SOFTWARE ENGINEER WHO LOVES SOLVING PROBLEMS. </span><br /> Specializing in JavaScript,NodeJS React/Redux, PostgreSQL and HTML/CSS. 
-           <br /> I'm always looking to improve my skills, whether that's adding features to my existing projects, developing new ones from scratch, or learning a new language/library/framework.</div>
+           <div className="about-info">I am a <span className="info-intro">SOFTWARE ENGINEER </span>specializing in JavaScript, NodeJS, React/Redux, PostgreSQL, HTML/CSS & more. 
+           <br /> I'm always finding ways to improve my skills, whether that's adding features to my existing projects, developing new ones from scratch, or learning a new language/library/framework.</div>
            <div className="about-photo">
            <img alt="" className="my-picture" src={myPicture}></img>
            </div>
@@ -16,5 +19,7 @@ function About() {
         </div>
     )
 }
+
+//I'm always looking to improve my skills, whether that's adding features to my existing projects, developing new ones from scratch, or learning a new language/library/framework
 
 export default About
